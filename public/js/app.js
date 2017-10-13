96257cdfd3a50d83ff9ec46a4fbcb9360ac08cb1
ewill20 <ewill2020@gmail.com>
2017-10-13 14:18:44 -0500

    fixing some js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
-
+$(document).ready(function() {
+    $(".back").hide();
+    $(".back2").hide();
+})
 
 $(".flipper").flip({
     trigger: 'manual'
@@ -8,12 +11,36 @@ $(".flipper").flip({
 $(".btn-arrow-left").click(function(){
     $(".flipper").flip({reverse: true});
     $(".flipper").flip('toggle');
-    $(".back2").show(); 
+    $(".back2").show();
+    $(".btn-arrow-left").on("toggle", function() {
+        $(".back2").hide();
+    })
     });
 
 
 $(".btn-arrow-right").click(function() {
     $(".flipper").flip({reverse: false});
     $(".flipper").flip('toggle');
-    
+    $(".back").show();
+    $(".btn-arrow-right").on("toggle", function() {
+        $('.back').hide();
+    })
 });
+
+// $(".btn-arrow-left").on("click", function() {
+//     display();
+
+// $(".btn-arrow-right").on("click", function() {
+//     display();
+// })    
+
+// function display() {
+//     if(".btn-arrow-left") {
+//         $(".back").html();
+//     } 
+//     if(".btn-arrow-right") {
+//         $(".back2").html();
+//     }
+// };
+
+// })

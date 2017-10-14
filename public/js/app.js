f4db6ffd2044efeb127f3e90b6426289c3f14582
teenagegrandma <rebecca.preston90@gmail.com>
2017-10-14 11:47:54 -0500

    typo fix

--- public/js/app.js
+++ public/js/app.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
 			"<input type='text' class='edit' style='display: none;'>",
 			"<button class='delete btn btn-default'>x</button>",
 			"</li>"
-			].join(""0)
+			].join("")
 		);
 
 		$newInputRow.find("button.delete").data("id", defriend.id);

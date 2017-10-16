c4c61b16ed1ff90aa1097b43901244181218b4bb
ewill20 <ewill2020@gmail.com>
2017-10-16 12:50:51 -0500

    flip working with alternate content on back

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,31 @@
 $(document).ready(function() {
     $(".back").hide();
     $(".back2").hide();
-
 });
-
-$(".flipper").flip({
-    trigger: 'manual'
-
+    $(".flipper").flip({
+        trigger: 'manual'
 });
-
-$(".btn-arrow-left").click(function(){
-    $(".flipper").flip({reverse: true});
-    $(".flipper").flip('toggle');
-    $(".back2").show(".different-Options");
-    $(".btn-arrow-left").on("toggle", function() {
-        $(".back2").hide();
-        
-       
-    })
-    });
-
-
 $(".btn-arrow-right").click(function() {
+    $(".back2").hide();
+    $(".back").show(".different-hints");
     $(".flipper").flip({reverse: false});
     $(".flipper").flip('toggle');
-    $(".back").show(".different-hints");
+    
     $(".btn-arrow-right").on("toggle", function() {
         $('.back').hide();
+        $(".back2").hide();
         
     })
 });
 
-// $(".btn-arrow-left").on("click", function() {
-//     display();
-
-// $(".btn-arrow-right").on("click", function() {
-//     display();
-// })    
-
-// function display() {
-//     if(".btn-arrow-left") {
-//         $(".back").html();
-//     } 
-//     if(".btn-arrow-right") {
-//         $(".back2").html();
-//     }
-// };
-
-// })
+    $(".btn-arrow-left").click(function() {
+        $(".back").hide();
+        $(".flipper").flip({reverse: true});
+        $(".flipper").flip('toggle');
+        $(".back2").show(".different-Options");
+        $(".btn-arrow-left").on("toggle", function() {
+            $(".back2").hide();
+            $(".back").hide();  
+    })
+    });
+   

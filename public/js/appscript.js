d243b529f9d7229ce7305f6635cf497725aace29
mhoppenrath <mmhoppenrath@gmail.com>
2017-10-17 12:48:10 -0500

    created the fb scriptss in the index page, holding till some front is done

--- public/js/appscript.js
+++ public/js/appscript.js
@@ -17,7 +17,80 @@ window.fbAsyncInit = function() {
  }(document, 'script', 'facebook-jssdk'));
 
 
- $("#loginButton").click(function() {
+//picks a random friend
+function friend() {
+  FB.api(
+      //makes a call to pull a list of names and id
+      "/{friend-list-id}",
+      function (response) {
+        if (response && !response.error) {
+          //the path to the summery count
+          var friendCount = response.summery.totalcount;
+          console.log(friendCount);
+          //picks a random friend id through on the list
+          defriendID = Math.floor(Math.random() * friendCount);
+          console.log(defriendID);
+          randoInfo(defriendID);
+
+        }
+      }
+    );
+};
+//pulls their info
+function randoInfo(defriendID){
+  FB.api(
+    //pulls the defriendID on the specific fields below
+    '/'+defriendID,
+    'GET',
+    //picture is public (it's the profile picture), profile picture call is permission gated
+    {"fields":"likes,hometown,about,picture"},
+    function(response) {
+      //stores the response as an object
+      var randoInfo = response;
+      return randoInfo;
+    }
+  );
+};
+//posts on the feed, we also do NOT have persmission for that right now.
+function feedPost(defriendArray){
+  var badPeople;
+  //for loop to put in together
+    for (var i = 0; i < defriendArray.length; i++) {
+      if (i=0) {
+      badPeople += defriendArray[i].name;
+      }
+      else {
+        badPeople += ", ";
+        badPeople += defriendArray[i].name;
+      }
+    }
+    //the specific method to post
+    FB.ui(
+      {
+        //method is where it goes
+        method: 'feed',
+        //these various fields I'm not sure are
+        name: 'Defriender',
+        link: '',
+        picture: 'https://scontent.flnk1-1.fna.fbcdn.net/v/t39.2081-6/c0.0.129.129/p128x128/21880224_127988504589917_7438022567916470272_n.png?oh=befbbe608e57d8edfa519f5f8265c611&oe=5A6DE249',
+        caption: '',
+        description: 'I should NOT be friends with these people: ' + badPeople,
+        message: ''
+      },
+      function(response) {
+        //tells us if it worked
+        if (response && response.post_id) {
+          alert('Post was published.');
+        } else {
+          alert('Post was not published.');
+        }
+      }
+    );
+};
+
+
+
+ $("#fbLogin").click(function() {
    FB.login(function(response) {
        if (response.authResponse) {
         console.log('Welcome!  Fetching your information.... ');
@@ -28,5 +101,12 @@ window.fbAsyncInit = function() {
         console.log('User cancelled login or did not fully authorize.');
        }
      console.log("trying to log on");
+   });
  });
- });
+//Right now this function goes to a dead end until a new person button is added
+ $("#newGuy").click(function() {
+   var packet = randoFriend();
+   $.post("/api/defriendList", function(packet, status){
+       console.log("Data: " + packet + "\nStatus: " + status);
+   });
+ })
